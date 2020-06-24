7d27f9e1f374aa0a99b7b8e26a2aa86a69a72a77
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-24 13:22:32 +0100

    Clean up item model

--- controllers/items-controller.js
+++ controllers/items-controller.js
@@ -2,29 +2,29 @@
 const Item = require('../models/item');
 
 // GET all Item Documents in the Collection
-module.exports.getItems = (res) => Item.getItems()
+module.exports.getItems = (res) => Item.find({})
     .then((items) => res.json({ 'success': true, "items": items }))
     .catch((err) => console.log(err))
 
 // GET single Item Document from the Collection
-module.exports.getItem = (req, res) => Item.getItem(req.params.id)
+module.exports.getItem = (req, res) => Item.findById(req.params.id)
     .then((item) => res.json(item))
     .catch((err) => console.log(err))
 
 // POST new Item Document to the Collection
-module.exports.postItem = (req, res) => Item.postItem(req.body)
+module.exports.postItem = (req, res) => Item.create(req.body)
     .then((item) => res.json({ 'success': true, "item": item }))
     .catch((err) => console.log(err))
 
 // PUT existing Item Document from the Collection
 module.exports.putItem = (req, res) =>
-    Item.getItem(req.params.id)
-        .then((item) => Item.putItem(req.params.id, { ...item._doc, ...req.body })
+    Item.findById(req.params.id)
+        .then((item) => Item.updateOne({ _id: req.params.id }, { ...item._doc, ...req.body })
             .then(() => res.json({ 'success': true, "updatedItem": { ...item._doc, ...req.body } }))
             .catch((err) => console.log(err))
         ).catch((err) => console.log(err))
 
 // DELETE single Item Document from the Collection
-module.exports.deleteItem = (req, res) => Item.deleteItem(req.params.id)
+module.exports.deleteItem = (req, res) => Item.deleteOne({ _id: req.params.id })
     .then(() => res.json({ "success": true }))
     .catch((err) => console.log(err))
--- models/item.js
+++ models/item.js
@@ -13,19 +13,4 @@ const ItemSchema = new mongoose.Schema({
 // define MongoDB Model
 const Item = mongoose.model('items', ItemSchema);
 
-// GET all MongoDB Documents in the MongoDB Collection
-module.exports.getItems = () => {
-    return Item.find({});
-};
-
-// GET single MongoDB Document in the MongoDB Collection
-module.exports.getItem = (id) => Item.findById(id)
-
-// PUT single MongoDB Document from the MongoDB Collection
-module.exports.putItem = (id, data) => Item.updateOne({ _id: id }, data)
-
-// POST new MongoDB Document to the MongoDB Collection
-module.exports.postItem = (data) => Item.create(data)
-
-// DELETE single MongoDB Document in the MongoDB Collection
-module.exports.deleteItem = (id) => Item.deleteOne({ _id: id })
+module.exports = Item
