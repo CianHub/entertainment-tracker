8afba733d3f36e18353ff01e4e51aadf7c6a2221
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-25 12:53:38 +0100

    Update sonarqube config

--- sonarqube-config.js
+++ sonarqube-config.js
@@ -7,7 +7,7 @@ sonarqubeScanner(
             'sonar.sources': '',
             'sonar.tests': 'tests',
             'sonar.inclusions': '**',
-            'sonar.exclusions': '**/tests, **/node_modules, **/jest-mongodb-config.js, */jest.config.js, **/.json, **/.spec.js, **/.yml',
+            'sonar.exclusions': '**/auth, **/tests, **/node_modules, jest-mongodb-config.js, jest.config.js, **/.json, **/.spec.js, **/.yml',
             'sonar.test.inclusions': '*.spec.js',
             'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
             'sonar.projectDescription': 'A web app for tracking the entertainment consumption of the user.',
