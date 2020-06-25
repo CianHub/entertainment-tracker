aebc99d3916576e194d720fc78f8f40a690f0176
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-25 19:33:10 +0100

    Remove unnecessary server code

--- server.js
+++ server.js
@@ -1,44 +1,23 @@
 const expressApp = require("./app");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Enable env variables to be read from .env file
 require('dotenv').config();
 
-// Start the server
-const configServer = async (mode) => {
-    if (mode === "test") {
-        try {
-            // Connect to Test DB
-            await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
-                if (err) {
-                    console.error(err);
-                    process.exit(1);
-                }
-            });
-        } catch (err) {
-            console.log(err)
-        }
-    } else if (mode === 'dev') {
-        try {
-            // Connect to DB
-            await mongoose
-                .connect(
-                    `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0-puzya.mongodb.net/${process.env.MONGO_DBNAME}`,
-                    { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
-                        if (err) {
-                            console.error(err);
-                            process.exit(1);
-                        }
-                    })
-            console.log('DEV MODE: DB connected')
-        } catch (err) {
-            console.log(err)
-        }
-    }
-}
+// Connect to DB
+mongoose
+    .connect(
+        `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0-puzya.mongodb.net/${process.env.MONGO_DBNAME}`,
+        { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        })
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.log(err))
 
-const startServer = async (app, mode = 'dev') => {
-    configServer(mode)
-    app.listen(process.env.PORT, () => console.log(`app running on PORT: ${process.env.PORT}`));
-}
+// Start the server
+const startServer = async (app) => app.listen(process.env.PORT, () => console.log(`app running on PORT: ${process.env.PORT}`));
+
 module.exports = startServer(expressApp)
