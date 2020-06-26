089adc77f99f8c9bc335a17404fb1f9516878a52
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-26 21:14:48 +0100

    Improve auth

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,76 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { token: null, form: { name: null, password: null } };
-  }
+export const Login = (props) => {
+  const [token, setToken] = useState(null);
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleNameChange = (event) => {
-    this.setState({
-      form: { name: event.target.value, password: this.state.form.password },
-    });
-  };
-  handlePasswordChange = (event) => {
-    this.setState({
-      form: { name: this.state.form.name, password: event.target.value },
-    });
-  };
-
-  handleSubmit = (event) => {
-    this.handleLogin();
+  const handleSubmit = (event) => {
+    handleLogin();
     event.preventDefault();
   };
 
-  handleLogin = async () => {
-    // TODO: Store token in Redux
-    // TODO: Redirect to entry list page
+  const handleLogin = async () => {
     try {
       const res = await axios.post(`http://localhost:5000/auth/local`, {
-        ...this.state.form,
+        name,
+        password,
       });
       if (res.data.token) {
-        this.setState({ token: res.data.token });
+        setToken(res.data.token);
       }
-      console.log(res.data.token);
     } catch (err) {}
   };
 
-  render() {
-    return (
-      <div className="login">
-        <h1>Login</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="name"
-              className="form-control"
-              id="name"
-              aria-describedby="nameHelp"
-              placeholder="Enter name"
-              value={this.state.name}
-              onChange={this.handleNameChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password"
-              value={this.state.password}
-              onChange={this.handlePasswordChange}
-            />
-          </div>
+  return (
+    <div className="login">
+      <br></br>
+      <h3>Login</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+            aria-describedby="nameHelp"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
