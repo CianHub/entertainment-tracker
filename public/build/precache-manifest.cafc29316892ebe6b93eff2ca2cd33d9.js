d95dea691829561b6efa1019248a146df8731f89
Cian O'Gralaigh <ihatecian@gmail.com>
2020-07-03 17:26:22 +0100

    Update build

---
+++ public/build/precache-manifest.cafc29316892ebe6b93eff2ca2cd33d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6098627a09d59113abb8394cf1e325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb6267ff5f8c10e65c06",
+    "url": "/static/css/main.34842377.chunk.css"
+  },
+  {
+    "revision": "ff86444b3c74a3c40f00",
+    "url": "/static/js/2.211286a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.211286a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6267ff5f8c10e65c06",
+    "url": "/static/js/main.7dd0c208.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5660ca1003ee73d5dce0338d836ac69f",
+    "url": "/static/media/scroll.5660ca10.png"
+  }
+]);
