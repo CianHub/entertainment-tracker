33301b965dfd73e59463bd35c60e38163b28a7c6
Cian O'Gralaigh <ihatecian@gmail.com>
2020-07-03 14:36:59 +0100

    Remove logs add procfile

---
+++ public/precache-manifest.81de7906b1a537216d7499fd5af4cadc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2575871960495168c33349e2cba4693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb06032864d28b492be",
+    "url": "/static/css/main.34842377.chunk.css"
+  },
+  {
+    "revision": "a2e8f50a5ed24d115236",
+    "url": "/static/js/2.93196a56.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.93196a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb06032864d28b492be",
+    "url": "/static/js/main.deb64ec7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5660ca1003ee73d5dce0338d836ac69f",
+    "url": "/static/media/scroll.5660ca10.png"
+  }
+]);
